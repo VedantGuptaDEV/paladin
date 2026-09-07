ccdd5599d6a2fc38b0ad8367cb3252477170faab
VedantGuptaDEV <vedantgupta2807@gmail.com>
2026-09-08 00:41:54 +0530

    ...

--- Engine/Risk_Engine/Tier2_Plotting.py
+++ Engine/Risk_Engine/Tier2_Plotting.py
@@ -2,7 +2,7 @@ import numpy as np
 from sentence_transformers import SentenceTransformer
 import pandas as pd
 
-mpropmts_df=pd.read_csv("assets\\malicious_prompts_data_plus_500.csv")
+mpropmts_df=pd.read_csv("Engine\\assets\\malicious_prompts_data_plus_700.csv")
 MAL_PROMPTS=mpropmts_df["prompt"].tolist()  # list of malicious prompts
 
 print(f"encoding {len(MAL_PROMPTS)} prompts...")
@@ -12,5 +12,5 @@ model=SentenceTransformer("all-MiniLM-L6-v2")
 # normalize_embeddings makes strings into unit vectors of 384 hyperparameters:
 vector_embedding=model.encode(MAL_PROMPTS,normalize_embeddings=True,show_progress_bar=True)
 
-np.save("assets\\embedded_mal_prompts.npy",vector_embedding)
+np.save("Engine\\assets\\embedded_mal_prompts.npy",vector_embedding)
 print("\ndone")
---
+++ Engine/reamdeeee.md
@@ -0,0 +1,3 @@
+# reamdeeee
+
+yes.
