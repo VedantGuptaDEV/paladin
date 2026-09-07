f0190548705263f79f94920f84d58127d9df8454
VedantGuptaDEV <vedantgupta2807@gmail.com>
2026-09-07 11:47:23 +0530

    trying

--- Engine/Backend.py
+++ Engine/Backend.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import Risk_Engine.RiskEngine as risk
 
-uin=pd.read_csv("")
+uin=pd.read_csv("engine\\Context_Engine\\trialHack_output.csv")
 
-prompt=uin["prompt"]
+prompt=uin["raw_prompt"].tolist()[-1]
 
 #Risk score:
 risk_fns=risk.risk_score(prompt)
+print("risk score:",risk_fns[1])
 
 #tier 2 check:
 t2_out=risk.tier2(risk_fns[0])
